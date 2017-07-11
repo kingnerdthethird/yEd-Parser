d861ea154a20637024bcb51387653eb48f509eb2
kingnerdthethird <kingnerdthethird@outlook.com>
2017-07-11 16:42:57 -0600

    Eh

--- yEd Parser/Diverter.cpp
+++ yEd Parser/Diverter.cpp
@@ -1,12 +1,13 @@
 #include "stdafx.h"
 #include "Diverter.h"
+#include <iostream>
+#include <sstream>
+using namespace std;
 
+Diverter::Diverter(){
 
-Diverter::Diverter()
-{
 }
 
+Diverter::~Diverter(){
 
-Diverter::~Diverter()
-{
 }
